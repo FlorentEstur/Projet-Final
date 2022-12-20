b17b1ad03d4030f81cb636ad7f59ffd563a2d1b2
FlorentEstur <warcr@DESKTOP-3VUL9G4>
2022-12-20 12:25:38 +0100

    Ajout des noms de tables

--- src/main/java/org/inti/model/Admin.java
+++ src/main/java/org/inti/model/Admin.java
@@ -8,7 +8,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table
+@Table(name="AdminProjet")
 public class Admin {
 	
 	@javax.persistence.Id
--- src/main/java/org/inti/model/Client.java
+++ src/main/java/org/inti/model/Client.java
@@ -8,7 +8,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table
+@Table(name="ClientProjet")
 public class Client {
 
 	@Id
